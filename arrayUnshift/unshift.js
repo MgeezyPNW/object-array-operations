5a7c5e52b3824844a12f629492fbadb7aa3e5bd8
michaelageorge <mgeorge9492@gmail.com>
2019-11-24 19:26:41 -0800

    stuck on figuring out how to shift items over in the array

--- arrayUnshift/unshift.js
+++ arrayUnshift/unshift.js
@@ -1,7 +1,25 @@
 'use strict';
 
+// This file will contain a working Array.unshift function, as well as a function that demonstrates how Array.unshift() works. 
+
+// What is Array.unshift()? What does it do?
+// Array.unshift() is a lot like push, in that it adds elements to an array. It differs from push however because it adds the elements to the beginning of the array. 
+
+// How does Array.unshift() work...?
+// Array.unshift() 
+
 let friends = ['Kate', 'Cole', 'Austin', 'Chewy'];
 
 friends.unshift('Bailey', 'Thomas');
 // Should return 6
 // Friends becomes ['Bailey', 'Thomas', 'Kate', 'Cole', 'Austin', 'Chewy'];
+
+let books = ['Tools of Titans', 'Tribe of Mentors', '4 Hour Work Week', '4 Hour Chef', '4 Hour Body'];
+
+function unshift(array, element) {
+  array.length = array.length + 1; 
+  for(let i=array.length; i<0; i--) {
+    array[i - 1] = array[i];
+  }
+  console.log(array);
+};

89938626e82d9d3660b1d82b115e11486679a5fa
michaelageorge <mgeorge9492@gmail.com>
2019-11-24 17:01:18 -0800

    learning about unshift()

---
+++ arrayUnshift/unshift.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let friends = ['Kate', 'Cole', 'Austin', 'Chewy'];
+
+friends.unshift('Bailey', 'Thomas');
+// Should return 6
+// Friends becomes ['Bailey', 'Thomas', 'Kate', 'Cole', 'Austin', 'Chewy'];

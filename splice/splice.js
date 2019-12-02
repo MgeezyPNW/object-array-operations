18846a5adbb44ba1994b81f1c6119689a605f8e4
michaelageorge <mgeorge9492@gmail.com>
2019-12-02 08:40:52 -0800

    learning/brainstorming splice

---
+++ splice/splice.js
@@ -0,0 +1,8 @@
+// Splice() changes the contents of an array by removing or replacing existing elements and/or adding new elements in place.
+// array.splice(a, b, c);
+  // Where a is the number of the index location on the array.    At this location, the element is replaced or removed. If       argument b is equal to zero, argument a replaces the value at  that spot, and everything from that point is pushed right. IF  argument b is 1, then argument a replaces the value at that    index spot and the value is removed. If b is 2, then the next  two values are removed, and so on. 
+    // Where b is how many items will be removed. And it removes from the right side of argument a, n spots.
+    // Where c is the element(s) being placed.
+
+// It will output an array containing the deleted items. If no items were deleted, it will return an empty array. Will mutate original array by changing the length. The length change will depend on how many elements are being added/removed. 
+    
---
+++ vocabulary.md
@@ -0,0 +1,3 @@
+In-Place Algorithm: An algorithm that transforms input. The input is overwritten/transformed/mutated by the output.
+
+Spread Operator: 
